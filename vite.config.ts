f681c9f381417218ad968ca03281d90ba0777843
gergogyulai <113334790+gergogyulai@users.noreply.github.com>
2025-03-30 20:27:39 +0200

    Add PWA support with vite-plugin-pwa and update favicon links in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePWA } from 'vite-plugin-pwa'
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,6 +12,9 @@ export default defineConfig({
     TanStackRouterVite({ target: "react" }),
     tailwindcss(),
     react(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
   ],
   resolve: {
     alias: {
